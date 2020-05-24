1b9d71a0e103cf2eef187d9626c7061b9ff844ba
polmes <pol.developer1@gmail.com>
2020-05-23 20:27:25 -0600

    Comment out unnecessary bits

--- mount-iphone.sh
+++ mount-iphone.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Init
-export PKG_CONFIG_PATH="${HOME}/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
-export CPATH="${HOME}/usr/include:${CPATH}"
-export MANPATH="${HOME}/usr/share/man:${MANPATH}"
-export PATH="${HOME}/usr/bin:${PATH}"
-export LD_LIBRARY_PATH="${HOME}/usr/lib:${LD_LIBRARY_PATH}"
+#export PKG_CONFIG_PATH="${HOME}/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
+#export CPATH="${HOME}/usr/include:${CPATH}"
+#export MANPATH="${HOME}/usr/share/man:${MANPATH}"
+#export PATH="${HOME}/usr/bin:${PATH}"
+#export LD_LIBRARY_PATH="${HOME}/usr/lib:${LD_LIBRARY_PATH}"
 
 # Try to mount
 idevicepair pair
